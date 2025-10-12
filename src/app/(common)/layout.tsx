38631663bd3ad3a9e28deb666062e7a4d2600e26
kimguny <kimguny12@gmail.com>
2025-10-12 21:34:32 +0900

    FIX: 레이아웃 대소문자 수정

--- src/app/(common)/layout.tsx
+++ src/app/(common)/layout.tsx
@@ -1,4 +1,4 @@
-import Layout from "@/components/Layout";
+import Layout from "@/components/layout";
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
