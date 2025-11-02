a42fbdf898a6ba3f553703552efa8f3541117806
kimguny <kimguny12@gmail.com>
2025-11-02 18:04:25 +0900

    FIX: 빌드 오류 수정

--- src/app/(common)/save/nfc/manage/[nfcId]/edit/page.tsx
+++ src/app/(common)/save/nfc/manage/[nfcId]/edit/page.tsx
@@ -1,17 +1,13 @@
-// src/app/(common)/save/nfc/manage/[nfcId]/edit/page.tsx
 import NfcRegisterForm from '@/components/nfc/NfcRegisterForm';
 import React from 'react';
 
-// [1. 수정] Props 타입이 params를 직접 받도록 수정
 interface EditPageProps {
-  params: {
+  params: Promise<{
     nfcId: string;
-  };
+  }>;
 }
 
-// [2. 수정] async/await 제거 (데이터 페칭은 폼 컴포넌트에서)
-export default function NfcEditPage({ params }: EditPageProps) {
-  
-  // [3. 수정] nfcId prop을 폼 컴포넌트로 전달
-  return <NfcRegisterForm mode="edit" nfcId={params.nfcId} />;
+export default async function NfcEditPage({ params }: EditPageProps) {
+  const resolvedParams = await params;
+  return <NfcRegisterForm mode="edit" nfcId={resolvedParams.nfcId} />;
 }
